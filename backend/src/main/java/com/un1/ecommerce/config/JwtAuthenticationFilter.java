f43708419ee6a64a9611c970cd917c27f8b18863
Tuskrus <decom321456@gmail.com>
2026-03-22 10:46:46 +0700

    feat(auth): hoàn thành JWT authentication, phân quyền Admin và tự động tạo Roles

--- backend/src/main/java/com/un1/ecommerce/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/un1/ecommerce/config/JwtAuthenticationFilter.java
@@ -1,11 +1,7 @@
 package com.un1.ecommerce.config;
 
-import com.un1.ecommerce.util.JwtUtil;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,7 +11,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import com.un1.ecommerce.util.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
